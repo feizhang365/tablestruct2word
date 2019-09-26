439b3338f8f82c5430601eaec2a6383336571995
fzh <feizhang365@gmail.com>
2019-09-26 17:40:45 +0800

    optimize table style
    optimize table style
    add table comment

--- src/gendocx.py
+++ src/gendocx.py
@@ -3,6 +3,7 @@ __author__ = 'FeiZhang 81597228@qq.com'
 __date__ = '2019-07-20'
 
 from docx import Document
+import datetime
 
 
 def gen_doc(title, author):
@@ -10,19 +11,21 @@ def gen_doc(title, author):
     generator document structure
     :return:
     """
+    currentDT = datetime.datetime.now()
+
     document = Document()
 
     document.add_heading(title, 0)
     # document.add_heading('The data table structure description', 0)
 
-    document.add_paragraph('author:{} '.format(author))
-    document.add_paragraph('date:{}'.format(__date__))
+    document.add_paragraph('author:{}       date:{}'.format(author, currentDT.strftime("%Y-%m-%d")))
+    # document.add_paragraph('')
     document.add_paragraph('\n')
 
     return document
 
 
-def doc_append_table(doc, tb_info, tb_name):
+def doc_append_table(doc, tb_info, tb_name, tb_comment):
     """
     append data table structure info to the document
     :param doc:
@@ -30,10 +33,10 @@ def doc_append_table(doc, tb_info, tb_name):
     :param tb_name:
     :return:
     """
-    table = doc.add_table(rows=1, cols=4)
+    table = doc.add_table(rows=1, cols=4, style='Table Grid')
     tbinfo_cells = table.rows[0].cells
     tbinfo_cells[0].merge(tbinfo_cells[1]).merge(tbinfo_cells[2]).merge(tbinfo_cells[3])
-    tbinfo_cells[0].text = '表名:{}'.format(tb_name)
+    tbinfo_cells[0].text = '表名:{}  {}'.format(tb_name, tb_comment)
     # tbinfo_cells = table.rows[1].cells
     # tbinfo_cells[0].merge(tbinfo_cells[1]).merge(tbinfo_cells[2]).merge(tbinfo_cells[3])
     # tbinfo_cells[0].text = '表描述:{}'.format(tb_name)
@@ -56,7 +59,6 @@ def doc_append_table(doc, tb_info, tb_name):
         row_cells[3].text = str(r[8])
 
     # add space between table
-    doc.add_paragraph("\n")
     doc.add_paragraph("\n")
 
 
